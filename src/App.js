512f2862f608d445b8cd23fb97712d9e22f25ae1
coolcrazyfox <86570262+coolcrazyfox@users.noreply.github.com>
2022-10-09 00:51:06 +0300

    created btn

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import  s  from './styles/Main.module.css'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={s.main_form}>
+        <div className={s.main_box}>
+            <div className={s.title}>Карточка студента</div>
+            <div className={s.information}> No data</div>
+            <div>
+                <button className={s.btn}>Добавить</button>
+            </div>
+
+        </div>
+
+
     </div>
   );
 }
