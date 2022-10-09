630141059f23ef263233290f04a7cb3a0319f322
coolcrazyfox <86570262+coolcrazyfox@users.noreply.github.com>
2022-10-09 22:20:19 +0300

    use redux toolkit slice

--- src/App.js
+++ src/App.js
@@ -1,27 +1,43 @@
 import  s  from './styles/Main.module.css'
 import {useDispatch, useSelector} from 'react-redux'
 import './App.css';
-import {decrement, increment} from "./toolkitRedux/toolkitReducer";
+import {addTodo, decrement, increment, removeLastTodo} from "./toolkitRedux/toolkitSlice";
+// import {decrement, increment} from "./toolkitRedux/toolkitReducer";
 // import {addTodo, decrement, increment, removeLastTodo} from "./redux/mainReducer";
+
+
+const addAsyncTodo =()=>{
+    return async dispatch =>{
+        setTimeout(()=>{
+            dispatch(addTodo("ASYNC TODO"))
+
+        }, 3000)
+    }
+}
 
 function App() {
     const count = useSelector(state => state.toolkit.count)
+    const todos = useSelector(state => state.toolkit.todos)
     // const count = useSelector(state => state.main.count)
     // const todos = useSelector(state => state.main.todos)
     const dispatch = useDispatch()
 
   return (
     <div className={s.main_form}>
-        <h1>Count {count}</h1>
-        <button onClick={()=>dispatch(increment())}>Increment</button>
-        <button onClick={()=>dispatch(decrement())}>Decrement</button>
-        {/*<button onClick={()=>dispatch(removeLastTodo())}>Delete last todo</button>*/}
-        {/*<button onClick={()=>dispatch(addTodo())}>  Add todo</button>*/}
-        {/*<ul>*/}
-        {/*    {todos.map(t=>*/}
-        {/*        <li key={t}>{t}</li>*/}
-        {/*    )}*/}
-        {/*</ul>*/}
+        <div>
+            <h1>Count {count}</h1>
+            <button onClick={()=>dispatch(increment())}>Increment</button>
+            <button onClick={()=>dispatch(decrement())}>Decrement</button>
+            <button onClick={()=>dispatch(removeLastTodo())}>Delete last todo</button>
+            <button onClick={()=>dispatch(addTodo(prompt()))}>  Add todo</button>
+            <button onClick={()=>dispatch(addAsyncTodo())}>  Add async todo</button>
+            <ul>
+                {todos.map(t=>
+                    <li key={t}>{t}</li>
+                )}
+            </ul>
+        </div>
+
         <div className={s.main_box}>
             <div className={s.title}>Карточка студента</div>
             <div className={s.information}> No data</div>
--- src/toolkitRedux/index.js
+++ src/toolkitRedux/index.js
@@ -1,9 +1,11 @@
 import {combineReducers, configureStore} from "@reduxjs/toolkit";
 import toolkitReducer from "./toolkitReducer";
+import toolkitSlice from "./toolkitSlice";
 
 
 const rootReducer = combineReducers({
-    toolkit: toolkitReducer
+    // toolkit: toolkitReducer
+    toolkit: toolkitSlice
 })
 export const store = configureStore({
     reducer: rootReducer,
