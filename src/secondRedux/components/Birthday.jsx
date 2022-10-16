eb6c0c361baf38ab2d3d802622c2f84d9c876b2f
coolcrazyfox <86570262+coolcrazyfox@users.noreply.github.com>
2022-10-16 13:20:28 +0300

    changing "how many old man?"

--- src/secondRedux/components/Birthday.jsx
+++ src/secondRedux/components/Birthday.jsx
@@ -4,13 +4,34 @@ import {useSelector} from "react-redux";
 const Birthday = () => {
     const birth_year=useSelector(state=>state.user.birth_year)
     const thatYear = 2022
+    const one= 'год'
+    const two= 'года'
+    const five = 'лет'
     const sum = thatYear - birth_year
+    const getNoun=( sum ,one, two, five) => {
+        let n = Math.abs(sum);
+        n %= 100;
+        if (n >= 5 && n <= 20) {
+            return five;
+        }
+        n %= 10;
+        if (n === 1) {
+            return one;
+        }
+        if (n >= 2 && n <= 4) {
+            return two;
+        }
+        return five;
+    }
 
     return (
         <div style={{display:"flex", flexDirection:"row" , margin:"2px"}}>
             <div>
                 <label htmlFor="">Год рождения:</label>
-                <span style={{marginLeft:'10px'}}>{birth_year}&nbsp;&nbsp;&nbsp;({sum}&nbsp;years)</span>
+                <span style={{marginLeft:'10px'}}>{birth_year}&nbsp;&nbsp;&nbsp;
+                    ({sum}&nbsp;{getNoun (sum ,one, two, five)})
+
+                </span>
             </div>
         </div>
 
